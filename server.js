2d3422176bb93c80c12d3e8de8e4c35a51184742
Filipe Lopes <filiperocklopes@gmail.com>
2021-12-29 18:57:13 -0300

    update to functional settings

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 module.exports = ({ env }) => {
-    console.log('env:', env);
-    console.log(env('ADMIN_JWT_SECRET'));
-    console.log(env('NGINX_URL'), env('PORT'))
-    return {
-  host: env('HOST','0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('NGINX_URL', 'localhost'),
-  proxy: true,
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'), 
-    },
-  },
-}
+    const config = {
+        host: env('HOST','0.0.0.0'),
+        port: env.int('PORT', 1337),
+        admin: {
+            auth: {
+                 secret: env('ADMIN_JWT_SECRET'), 
+            },
+        },
+    }
+    console.log(config)
+    return config;
 };
